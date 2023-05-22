d42461060838a1a8d5f889d4951e317fe3876ce7
olivia <opierez@gmail.com>
2023-05-22 15:58:13 -0400

    final edits, cleaned up files

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Grab DOM Elements
+const form = document.getElementById('reservation-form')
+const button = document.querySelector('button');
+const inputFields = document.querySelectorAll('input')
+
+// Add Event Listeners
+form.addEventListener('submit', handleSubmit)
+
+// Handles user's form submission
+function handleSubmit(e) {
+    e.preventDefault()
+    console.log("I was clicked")
+
+    document.body.style.backgroundImage = "none"; // removes the background image
+    document.body.style.setProperty("background-color", "#952861", "important"); // changes the background color
+    button.classList.add('submitted'); // adds a class to the button to change the hover color
+    inputFields.forEach(input => input.classList.add('submitted')); // adds a class to the input fields to change hover border color
+
+    form.reset() // resets the form input fields 
+     
+}
